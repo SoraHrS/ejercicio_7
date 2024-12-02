4b71f18e817cfeeed3e25a5febf09e51e1eaf7ed
SoraHrS <soherreras@unal.edu.co>
2024-12-02 15:08:03 -0500

    se realizó el ejercicio 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Ingrese el valor de a: ");
+        a = sc.nextDouble();
+        System.out.println("Ingrese el valor de b: ");
+        b = sc.nextDouble();
+
+        if (a > b) {
+            System.out.println("A es mayor que B");
+        } else if (a == b) {
+            System.out.println("A es igual que B");
+        }else {
+            System.out.println("A es menor que B");
+        }
     }
 }
